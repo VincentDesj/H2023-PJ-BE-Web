7b000f9eb722fe2b1abc2aa31923960bc2a783ff
VincentDesj <37562907+VincentDesj@users.noreply.github.com>
2023-06-12 21:54:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(
 app.use(
     session({
         key: "LaBambaTriste",
-        resave: false,
+        resave: true,
         secret: "user",
         saveUninitialized: false,
         cookie: {
